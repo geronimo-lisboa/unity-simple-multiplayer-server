da058fdd15d90b292db258d2c162aad071a29da5
Geromin <luciano.geronimo.fnord@gmail.com>
2019-03-07 07:06:38 -0300

    1o commit

--- SimpleMultiplayerServer/Startup.cs
+++ SimpleMultiplayerServer/Startup.cs
@@ -6,7 +6,9 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-
+/// <summary>
+/// http://www.patrickschadler.com/creating-a-rest-webservice-with-net-core/
+/// </summary>
 namespace SimpleMultiplayerServer
 {
     public class Startup
